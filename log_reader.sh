38a114bbcffae6b0fbfa0cf147f86c9cebec06e3
Billaids <jimmy.nelle@hsw-stud.de>
2020-09-05 22:33:04 +0200

    Add Alienvault support
    *improve script
    *its possible to ban more than one ip per run
    
    Signed-off-by: Billaids <jimmy.nelle@hsw-stud.de>

--- log_reader.sh
+++ log_reader.sh
@@ -2,8 +2,9 @@
 
 TELEGRAM_TOKEN=""
 NGINX_LOG="/var/log/nginx/access.log"
-YOUR_API_KEY="" #Get your own KEY
+YOUR_API_KEY="" #Get your own ABUSEIPBDP-KEY
 IP_LOG="/root/ips.txt"
+ALIENVAULT_API_KEY="" #Get your own ALIENVAULT-KEY
 
 function sendTG() {
     curl -s "https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendmessage" --data "text=${*}&chat_id=358477256&disable_web_page_preview=true&parse_mode=Markdown" > /dev/null
@@ -11,40 +12,49 @@ function sendTG() {
 
 
 function read_file() {
-	export ip=$(tail -n 1 $NGINX_LOG | awk '{print $1}');
-	export time=$(tail -n 1 $NGINX_LOG | awk '{print $4 $5}')
+	mapfile -t ip < <(tail -n 20 $NGINX_LOG | awk '{print $1}'); #change here amount ips
+	mapfile -t time< <(tail -n 20 $NGINX_LOG | awk '{print $4 $5}') #same number as ips
 }
 
 function sendTGPQHAZ(){
     curl -s "https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendmessage" --data "text=${*}&chat_id=958877875&disable_web_page_preview=true&parse_mode=Markdown" > /dev/null
 }
 
+function alienvault(){
+	alien_output=$(curl https://otx.alienvault.com/api/v1/indicators/IPv4/${*}/reputation -H "X-OTX-API-KEY: $ALIENVAULT_API_KEY")
+	alien_score=$( echo $alien_output | jq '.reputation.threat_score')
+	malicious_host=$( echo $alien_output | jq '.reputation.counts."Malicious Host"' | sed 's/1/MALICIOUS HOST/g')
+}
+
 function abuseipdb() {
-	ip_check=$(grep $ip $IP_LOG | tail -n 1)
-	if ! [ -z $ip_check ]; then
-		ip_check=$ip
-	else
-		if  [[ "$ip" =~ $ip_check ]]; then
-			output=$(curl -G https://api.abuseipdb.com/api/v2/check --data-urlencode "ipAddress=${ip}" -d maxAgeInDays=90 -d verbose -H "Key: $YOUR_API_KEY" -H "Accept: application/json")
-			whitelist=$( echo $output | jq '.data.isWhitleisted')
-			score=$( echo $output | jq '.data.abuseConfidenceScore')
-			isp=$( echo $output | jq '.data.isp'| tr "&" " ")
-			countryname=$( echo $output | jq '.data.countryName')
-			totalreport= $( echo $output | jq '.data.totalReports')
-			lastreport=$( echo $output | jq '.data.lastReportedAt')
-			echo $ip >> $IP_LOG
-				if [ $score -gt 1 ]; then
-					block_ip $ip
-					sendTG "Successfully Blocked! %0AIP: $ip %0ACountry: $countryname %0AAbusescore: $score %0ALast Report: $lastreport"
-					sleep 2
-					sendTGPQHAZ "Successfully Blocked! %0AIP: $ip %0ACountry: $countryname %0AAbusescore: $score %0ALast Report: $lastreport %0ATotal Reports: $totalreport"
-				else
-					sendTG "IP: $ip %0ATime: $time %0AWhitelist: $whitelist %0AAbusescore: $score %0AISP: $isp %0ACountry: $countryname %0ATotal Reports: $totalreport %0ALast Report: $lastreport   "
-					sleep 2
-					sendTGPQHAZ "IP: $ip %0ATime: $time %0AWhitelist: $whitelist %0AAbusescore: $score %0AISP: $isp %0ACountry: $countryname %0ATotal Reports: $totalreport %0ALast Report: $lastreport   "
+	for ((i=0;i<${#ip[@]};i++));do
+		ip_check=$(grep $ip $IP_LOG | tail -n 1)
+		if ! [ -z $ip_check ]; then
+			ip_check=${ip[i]}
+		else
+			if  [[ "${ip[i]}" =~ $ip_check ]]; then
+				alienvault ${ip[i]}
+				output=$(curl -G https://api.abuseipdb.com/api/v2/check --data-urlencode "ipAddress=${ip[i]}" -d maxAgeInDays=90 -d verbose -H "Key: $YOUR_API_KEY" -H "Accept: application/json")
+				whitelist=$( echo $output | jq '.data.isWhitleisted')
+				score=$( echo $output | jq '.data.abuseConfidenceScore')
+				isp=$( echo $output | jq '.data.isp'| tr "&" " ")
+				countryname=$( echo $output | jq '.data.countryName')
+				totalreport= $( echo $output | jq '.data.totalReports')
+				lastreport=$( echo $output | jq '.data.lastReportedAt')
+				echo ${ip[i]} >> $IP_LOG
+					if [ $score -gt 1 ] || [ $alien_score -gt 0 ] ; then
+						block_ip ${ip[i]}
+						sendTG "Successfully Blocked! %0AIP: ${ip[i]} %0ATime: ${time[i]} %0ACountry: $countryname %0AAbusescore: $score %0ALast Report: $lastreport%0A %0A#############%0AAlienvault Results:%0A %0AThreat Score: $alien_score %0AMalicious: $malicious_host "
+						sleep 2
+						sendTGPQHAZ "Successfully Blocked! %0AIP: ${ip[i]} %0ATime: ${time[i]} %0ACountry: $countryname %0AAbusescore: $score %0ALast Report: $lastreport%0A %0A#############%0AAlienvault Results:%0a %0AThreat Score: $alien_score %0AMalicious: $malicious_host "
+					else
+						sendTG "IP: ${ip[i]} %0ATime: ${time[i]} %0AWhitelist: $whitelist %0AAbusescore: $score %0AISP: $isp %0ACountry: $countryname %0ATotal Reports: $totalreport %0ALast Report: $lastreport%0A %0A#############%0AAlienvault Results:%0A %0AThreat Score: $alien_score %0AMalicious: $malicious_host "
+						sleep 2
+						sendTGPQHAZ "IP: ${ip[i]} %0ATime: ${time[i]} %0AWhitelist: $whitelist %0AAbusescore: $score %0AISP: $isp %0ACountry: $countryname %0ATotal Reports: $totalreport %0ALast Report: $lastreport %0A#############%0AAlienvault Results:%0A %0AThreat Score: $alien_score %0AMalicious: $malicious_host "
+					fi
 				fi
-			fi
-	fi
+		fi
+	done
 }
 
 ########################################################
@@ -58,7 +68,7 @@ if ! [[ -w $IP_LOG ]]; then
 fi
 while true; do
 first_size=$(du -b /var/log/nginx/access.log | cut -f 1)
-sleep 1 
+sleep 0.5 
 second_size=$(du -b /var/log/nginx/access.log | cut -f 1) 
 if [[ $first_size != $second_size ]]; then
 	read_file
