a924819c6241d0a498eedf8669195b9e21a90ab6
Billaids <jimmy.nelle@hsw-stud.de>
2020-09-05 20:30:25 +0200

    improve log_reader
    Signed-off-by: Billaids <jimmy.nelle@hsw-stud.de>

--- log_reader.sh
+++ log_reader.sh
@@ -2,7 +2,6 @@
 
 TELEGRAM_TOKEN=""
 NGINX_LOG="/var/log/nginx/access.log"
-n=0
 YOUR_API_KEY="" #Get your own KEY
 IP_LOG="/root/ips.txt"
 
@@ -21,56 +20,30 @@ function sendTGPQHAZ(){
 }
 
 function abuseipdb() {
-	if [ $n -eq 0 ]; then
-		output=$(curl -G https://api.abuseipdb.com/api/v2/check --data-urlencode "ipAddress=${ip}" -d maxAgeInDays=90 -d verbose -H "Key: $YOUR_API_KEY" -H "Accept: application/json ")
-		whitelist=$( echo $output | jq '.data.isWhitleisted')
-		score=$( echo $output | jq '.data.abuseConfidenceScore')
-		isp=$( echo $output | jq '.data.isp'| tr "&" " ")
-		countryname=$( echo $output | jq '.data.countryName')
-		totalreport= $( echo $output | jq '.data.totalReports')
-		lastreport=$( echo $output | jq '.data.lastReportedAt')
-		ip1=$ip
-		ip_check=$(cat $IP_LOG | grep $ip | tail -n 1)
-		echo $ip
-		if  [[ $ip =~ $ip_check ]]; then
-			echo $ip >> $IP_LOG
-			if [ $score -gt 1 ]; then
-				block_ip $ip
-				sendTG "Successfully Blocked! %0AIP: $ip %0ACountry: $countryname %0AAbusescore: $score %0ALast Report: $lastreport %0ATotal Reports: $totalreport"
-				sleep 2
-				sendTGPQHAZ "Successfully Blocked! %0AIP: $ip %0ACountry: $countryname %0AAbusescore: $score %0ALast Report: $lastreport %0ATotal Reports: $totalreport"
-			else
-				sendTG "IP: $ip %0ATime: $time %0AWhitelist: $whitelist %0AAbusescore: $score %0AISP: $isp %0ACountry: $countryname %0ATotal Reports: $totalreport %0ALast Report: $lastreport   "
-				sleep 2
-				sendTGPQHAZ "IP: $ip %0ATime: $time %0AWhitelist: $whitelist %0AAbusescore: $score %0AISP: $isp %0ACountry: $countryname %0ATotal Reports: $totalreport %0ALast Report: $lastreport   "
-			fi
-		fi
-		$n=$((n++))
+	ip_check=$(grep $ip $IP_LOG | tail -n 1)
+	if ! [ -z $ip_check ]; then
+		ip_check=$ip
 	else
-	if [ $ip != $ip1 ]; then
-		output=$(curl -G https://api.abuseipdb.com/api/v2/check --data-urlencode "ipAddress=${ip}" -d maxAgeInDays=90 -d verbose -H "Key: $YOUR_API_KEY" -H "Accept: application/json")
-		whitelist=$( echo $output | jq '.data.isWhitleisted')
-		score=$( echo $output | jq '.data.abuseConfidenceScore')
-		isp=$( echo $output | jq '.data.isp'| tr "&" " ")
-		countryname=$( echo $output | jq '.data.countryName')
-		totalreport= $( echo $output | jq '.data.totalReports')
-		lastreport=$( echo $output | jq '.data.lastReportedAt')
-		ip1=$ip
-		ip_check=$(cat $IP_LOG | grep $ip | tail -n 1)
-		if [[ $ip =~ $ip_check ]]; then
+		if  [[ "$ip" =~ $ip_check ]]; then
+			output=$(curl -G https://api.abuseipdb.com/api/v2/check --data-urlencode "ipAddress=${ip}" -d maxAgeInDays=90 -d verbose -H "Key: $YOUR_API_KEY" -H "Accept: application/json")
+			whitelist=$( echo $output | jq '.data.isWhitleisted')
+			score=$( echo $output | jq '.data.abuseConfidenceScore')
+			isp=$( echo $output | jq '.data.isp'| tr "&" " ")
+			countryname=$( echo $output | jq '.data.countryName')
+			totalreport= $( echo $output | jq '.data.totalReports')
+			lastreport=$( echo $output | jq '.data.lastReportedAt')
 			echo $ip >> $IP_LOG
-			if [ $score -gt 1 ]; then
-				block_ip $ip
-				sendTG "Successfully Blocked! %0AIP: $ip %0ACountry: $countryname %0AAbusescore: $score %0ALast Report: $lastreport"
-				sleep 2
-				sendTGPQHAZ "Successfully Blocked! %0AIP: $ip %0ACountry: $countryname %0AAbusescore: $score %0ALast Report: $lastreport %0ATotal Reports: $totalreport"
-			else
-				sendTG "IP: $ip %0ATime: $time %0AWhitelist: $whitelist %0AAbusescore: $score %0AISP: $isp %0ACountry: $countryname %0ATotal Reports: $totalreport %0ALast Report: $lastreport   "
-				sleep 2
-				sendTGPQHAZ "IP: $ip %0ATime: $time %0AWhitelist: $whitelist %0AAbusescore: $score %0AISP: $isp %0ACountry: $countryname %0ATotal Reports: $totalreport %0ALast Report: $lastreport   "
+				if [ $score -gt 1 ]; then
+					block_ip $ip
+					sendTG "Successfully Blocked! %0AIP: $ip %0ACountry: $countryname %0AAbusescore: $score %0ALast Report: $lastreport"
+					sleep 2
+					sendTGPQHAZ "Successfully Blocked! %0AIP: $ip %0ACountry: $countryname %0AAbusescore: $score %0ALast Report: $lastreport %0ATotal Reports: $totalreport"
+				else
+					sendTG "IP: $ip %0ATime: $time %0AWhitelist: $whitelist %0AAbusescore: $score %0AISP: $isp %0ACountry: $countryname %0ATotal Reports: $totalreport %0ALast Report: $lastreport   "
+					sleep 2
+					sendTGPQHAZ "IP: $ip %0ATime: $time %0AWhitelist: $whitelist %0AAbusescore: $score %0AISP: $isp %0ACountry: $countryname %0ATotal Reports: $totalreport %0ALast Report: $lastreport   "
+				fi
 			fi
-		fi
-	fi
 	fi
 }
 
@@ -84,11 +57,11 @@ if ! [[ -w $IP_LOG ]]; then
 	touch $IP_LOG
 fi
 while true; do
-	first_size=$(du -b /var/log/nginx/access.log | cut -f 1)
-	sleep 3
-	second_size=$(du -b /var/log/nginx/access.log | cut -f 1)
-	if [[ $first_size != $second_size ]]; then
-		read_file
-		abuseipdb
-	fi
+first_size=$(du -b /var/log/nginx/access.log | cut -f 1)
+sleep 1 
+second_size=$(du -b /var/log/nginx/access.log | cut -f 1) 
+if [[ $first_size != $second_size ]]; then
+	read_file
+	abuseipdb
+fi
 done
